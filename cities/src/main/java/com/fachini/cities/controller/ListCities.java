a1e4a9dbd68eb99742e1344dac0e23b4e339d214
Anderson.Fachini <anderson.fachini@senior.com.br>
2017-04-03 13:11:30 -0300

    Implemented two more primitives

--- cities/src/main/java/com/fachini/cities/controller/ListCities.java
+++ cities/src/main/java/com/fachini/cities/controller/ListCities.java
@@ -6,12 +6,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fachini.cities.entities.city.City;
 import com.fachini.cities.entities.city.CityRepository;
 
 @RestController
-public class ListCitiesByState {
+public class ListCities {
 
 	@Autowired
 	private CityRepository cityRepository;
@@ -32,11 +34,8 @@ public class ListCitiesByState {
 	public List<StateCities> listStateManyFewerCities() {
 		List<StateCities> stateCities = new ArrayList<>();
 
-		String columnOrder = "amount";
-
-		List<Object[]> citiesPerState = cityRepository.getCitiesPerState(columnOrder);
-		Object[] stateLessCities = citiesPerState.get(0);
-		Object[] stateMoreCities = citiesPerState.get(citiesPerState.size() - 1);
+		Object[] stateLessCities = cityRepository.getFewerCitiesState().get(0);
+		Object[] stateMoreCities = cityRepository.getManyCitiesState().get(0);
 
 		stateCities.add(new StateCities((String) stateLessCities[1], (BigInteger) stateLessCities[0]));
 		stateCities.add(new StateCities((String) stateMoreCities[1], (BigInteger) stateMoreCities[0]));
@@ -48,12 +47,20 @@ public class ListCitiesByState {
 	public List<StateCities> listCitiesByState() {
 		List<StateCities> stateCities = new ArrayList<>();
 
-		String columnOrder = "name";
-
-		cityRepository.getCitiesPerState(columnOrder).forEach(result -> {
+		cityRepository.getCitiesByState().forEach(result -> {
 			stateCities.add(new StateCities((String) result[1], (BigInteger) result[0]));
 		});
 
 		return stateCities;
+	}
+	
+	@RequestMapping("/get-city-by-ibge-id")
+	public City getCityByIbgeId(@RequestParam(value="ibgeId") Long ibgeId) {
+		return cityRepository.findByIbgeId(ibgeId);
+	}
+	
+	@RequestMapping("/get-city-by-state-id")
+	public List<City> getCityByStateId(@RequestParam(value="stateId") String stateId) {
+		return cityRepository.findByStateIdOrderByName(stateId);
 	}
 }
--- cities/src/main/java/com/fachini/cities/entities/city/CityRepository.java
+++ cities/src/main/java/com/fachini/cities/entities/city/CityRepository.java
@@ -4,22 +4,23 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 public interface CityRepository extends CrudRepository<City, Long> {
 
 	@Query("SELECT c FROM City c WHERE c.capital IS TRUE ORDER BY c.name")
 	List<City> getCapitals();
 
-	/**
-	 * Get the amount of cities per state
-	 * 
-	 * @param indexOrder
-	 *            1 to order by amount of cities OR 2 to order by the name of
-	 *            state
-	 * @return
-	 */
-	@Query(value = "SELECT COUNT(c.*) as amount, s.name as name FROM City c JOIN State s ON c.state_id = s.id GROUP BY s.name ORDER BY :columnOrder", nativeQuery = true)
-	List<Object[]> getCitiesPerState(@Param("columnOrder") String columnOrder);
+	@Query(value = "SELECT COUNT(c.*) as amount, s.name as name FROM City c JOIN State s ON c.state_id = s.id GROUP BY s.name ORDER BY amount limit 1", nativeQuery = true)
+	List<Object[]> getFewerCitiesState();
+	
+	@Query(value = "SELECT COUNT(c.*) as amount, s.name as name FROM City c JOIN State s ON c.state_id = s.id GROUP BY s.name ORDER BY amount desc limit 1", nativeQuery = true)
+	List<Object[]> getManyCitiesState();
+	
+	@Query(value = "SELECT COUNT(c.*) as amount, s.name as name FROM City c JOIN State s ON c.state_id = s.id GROUP BY s.name ORDER BY name", nativeQuery = true)
+	List<Object[]> getCitiesByState();
+	
+	City findByIbgeId(Long ibgeId);
+	
+	List<City> findByStateIdOrderByName(String stateId);
 
 }
